dd8581887198d856b4ca545373356dda31e7f727
adem <adembaroudi3177@gmail.com>
2020-02-26 13:23:37 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+import { Comp4Component } from './comp4/comp4.component';
+import { Comp5Component } from './comp5/comp5.component';
+import { Comp6Component } from './comp6/comp6.component';
+import { Comp1Component } from './comp1/comp1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Comp2Component,
+    Comp3Component,
+    Comp4Component,
+    Comp5Component,
+    Comp6Component,
+    Comp1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
