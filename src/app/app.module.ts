ad197515e18126e5af18d643baea75467c5af427
adem <adembaroudi3177@gmail.com>
2020-02-26 16:34:00 +0100

    enable ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { Comp2Component } from './comp2/comp2.component';
 import { Comp3Component } from './comp3/comp3.component';
 import { Comp4Component } from './comp4/comp4.component';
 import { Comp5Component } from './comp5/comp5.component';
 import { Comp6Component } from './comp6/comp6.component';
 import { Comp1Component } from './comp1/comp1.component';
+import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { Comp1Component } from './comp1/comp1.component';
     Comp1Component
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
